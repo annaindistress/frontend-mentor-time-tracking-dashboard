b3eb78cb3e26096abe98f0b347aba3de19d4ef64
Anna Artemova <annartmva@gmail.com>
2023-08-25 14:45:43 +0400

    Initial

---
+++ src/components/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const App = () => {
+  const test = true;
+
+  return (
+    <main className="page__main">
+      <h1 className="sr-only">Time tracking dashboard</h1>
+    </main>
+  );
+};
+
+export default App;
